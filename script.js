7c2613a5e87afe56a07b0f3cda9a9e98467855f8
Addit <adam.garza33@hotmail.com>
2023-02-13 17:23:21 -0600

    used parseInt to parse the string and return an integer

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ var upperCase = ["A", "B", "C", "D", "E", "F", ]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", ]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ]
 
+function generatePassword() {
+
+}
+
+function createPrompts() {
+    passwordLength = parseInt(prompt("How many characters do you want your password to contain? 8-128 characters are required"));
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
